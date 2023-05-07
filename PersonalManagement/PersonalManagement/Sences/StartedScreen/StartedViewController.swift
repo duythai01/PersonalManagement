ea189aa125acdb32f2ef15d6365d621a936c0667
duythai01 <daoduythai1472001@gmail.com>
2023-05-07 11:42:27 +0700

    Create UI started

--- PersonalManagement/PersonalManagement/Sences/StartedScreen/StartedViewController.swift
+++ PersonalManagement/PersonalManagement/Sences/StartedScreen/StartedViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class StartedViewController: UIViewController {
+    @IBOutlet private weak var getStartedButton: UIButton!
 
     var viewModel: StartedViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configView()
+    }
+
+    private func configView()  {
+        getStartedButton.layer.cornerRadius = getStartedButton.frame.size.height / 2
     }
 
 }
